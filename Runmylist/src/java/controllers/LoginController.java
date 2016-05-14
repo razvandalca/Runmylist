20c09e9459e6cf46942b91b1d13cad5b16803c00
razvan.dalca <dalca.razvan@gmail.com>
2016-05-14 15:13:27 +0300

    BIG TIME REFACTORING PART 1

--- Runmylist/src/java/controllers/LoginController.java
+++ Runmylist/src/java/controllers/LoginController.java
@@ -22,41 +22,26 @@ public class LoginController extends HttpServlet {
     UserDAO userDAO = UserDAO.getInstance();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        processRequest(request, response);
-
-        // redirect to google for authorization
-        String clientID="701669709805-p3l6nr16a7obtl7qappnh0t3i27fl5i6.apps.googleusercontent.com";
-        StringBuilder oauthUrl = new StringBuilder().append("https://accounts.google.com/o/oauth2/auth")
-                .append("?client_id=").append(clientID) // the client id from the api console registration
-                .append("&response_type=code")
-                .append("&scope=openid%20email") // scope is the api permissions we are requesting
-                .append("&redirect_uri=http://localhost:8080/Runmylist/LoginView.jsp") // the servlet that google redirects to after authorization
-                .append("&access_type=offline") // here we are asking to access to user's data while they are not signed in
-                .append("&approval_prompt=force"); // this requires them to verify which account to use, if they are already signed in
-
-        response.sendRedirect(oauthUrl.toString());
-    }
-
-    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        switch(request.getParameter("login_type")){
+            case "default":
         String username = request.getParameter("uname");
         String password = request.getParameter("password");
         if (userDAO.isPasswordCorrect(username, password)) {
             // user exists
-            request.getSession().setAttribute("user", username);
-            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-            rd.forward(request, response);
+            out.print("ok");
         } else {
-            // does not exist
             String error = "User does not exist or password is incorrect";
             request.setAttribute("LOGIN_ERROR", error);
             RequestDispatcher rd = request.getRequestDispatcher("/LoginView.jsp");
             rd.forward(request, response);
         }
+        
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+
+
 
 }
