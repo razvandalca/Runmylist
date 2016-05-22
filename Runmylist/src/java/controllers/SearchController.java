3b083be773f532e77a1df45ef4b7b576ba1fa2c2
razvan.dalca <dalca.razvan@gmail.com>
2016-05-22 14:32:38 +0300

    youtube Search and sound cloud search , integration with frontend working

--- Runmylist/src/java/controllers/SearchController.java
+++ Runmylist/src/java/controllers/SearchController.java
@@ -15,6 +15,7 @@ import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.Thumbnail;
+import com.google.gson.Gson;
 import java.io.BufferedReader;
 import javax.servlet.http.HttpServlet;
 import java.io.IOException;
@@ -32,123 +33,129 @@ import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import de.voidplus.soundcloud.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import pojo.MyTrack;
 
 /**
  *
  * @author Razvan'S PC
  */
 public class SearchController extends HttpServlet {
-   
-        private static final String PROPERTIES_FILENAME = "youtube.properties";
 
-    private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
-    private static YouTube youtube;
-    public static final JsonFactory JSON_FACTORY = new JacksonFactory();
+    private static final String PROPERTIES_FILENAME = "youtube.properties";
 
-    /*
-     * Prompt the user to enter a query term and return the user-specified term.
-     */
-    private static String getInputQuery() throws IOException {
+    private  final long NUMBER_OF_VIDEOS_RETURNED = 50;
+    private  YouTube youtube;
+    public final JsonFactory JSON_FACTORY = new JacksonFactory();
 
-        String inputQuery = "Eminem";
-
-//        System.out.print("Please enter a search term: ");
-//        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-//        inputQuery = bReader.readLine();
-//
-//        if (inputQuery.length() < 1) {
-//            // Use the string "YouTube Developers Live" as a default.
-//            inputQuery = "YouTube Developers Live";
-//        }
-        return inputQuery;
-    }
-
-    private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
-        System.err.println();
-        System.out.println("\n=============================================================");
-        System.out.println(
-                "   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
-        System.out.println("=============================================================\n");
-
-        if (!iteratorSearchResults.hasNext()) {
-            System.out.println(" There aren't any results for your query.");
-        }
-
-        while (iteratorSearchResults.hasNext()) {
-
-            SearchResult singleVideo = iteratorSearchResults.next();
-            ResourceId rId = singleVideo.getId();
-
-            // Confirm that the result represents a video. Otherwise, the
-            // item will not contain a video ID.
-            if (rId.getKind().equals("youtube#video")) {
-                Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
-
-                System.out.println(" Video Id" + rId.getVideoId());
-                System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
-                System.out.println(" Thumbnail: " + thumbnail.getUrl());
-                System.out.println(" URL: " +"https://www.youtube.com/watch?v="+ rId.getVideoId());
-                System.out.println("\n-------------------------------------------------------------\n");
-            }
-        }
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            try (PrintWriter out = response.getWriter()) {
-            // This object is used to make YouTube Data API requests. The last
-            // argument is required, but since we don't need anything
-            // initialized when the HttpRequest is initialized, we override
-            // the interface and provide a no-op function.
-            
-            youtube = new YouTube.Builder(new NetHttpTransport(), JSON_FACTORY , new HttpRequestInitializer() {
-                @Override
-                public void initialize(HttpRequest request) throws IOException {
-                }
-            }).setApplicationName("youtube-cmdline-search-sample").build();
-
-            // Prompt the user to enter a query term.
-            String queryTerm = request.getParameter("imputQuery");
-
-            // Define the API request for retrieving search results.
-            YouTube.Search.List search = youtube.search().list("id,snippet");
-
-            // Set your developer key from the Google Developers Console for
-            // non-authenticated requests. See:
-            // https://console.developers.google.com/
-            String apiKey = "AIzaSyByS-IwVEMkiCuX2OXvBKdsywjR7Ls7828";
-            search.setKey(apiKey);
-            search.setQ(queryTerm);
-
-            // Restrict the search results to only include videos. See:
-            // https://developers.google.com/youtube/v3/docs/search/list#type
-            search.setType("video");
-
-            // To increase efficiency, only retrieve the fields that the
-            // application uses.
-            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-            search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
-
-            // Call the API and print results.
-            SearchListResponse searchResponse = search.execute();
-            List<SearchResult> searchResultList = searchResponse.getItems();
-            if (searchResultList != null) {
-                System.err.println(searchResponse.toString());
-                out.print(searchResponse.toString());
-                prettyPrint(searchResultList.iterator(), queryTerm);
+        try (PrintWriter out = response.getWriter()) {
+            ArrayList<MyTrack> resultArray=youtubeQuery(request.getParameter("inputQuery"));
+//            request.getParameter("inputQuery")
+//            resultArray.addAll(soundCloudQuery("Rihana"));
+            Collections.shuffle(resultArray);
+            JSONObject jSONObject;
+            JSONArray jSonArray = new JSONArray();
+            for (MyTrack myTrack : resultArray) {
+                jSONObject=new JSONObject();
+                jSONObject.put("author", myTrack.getAuthor());
+                jSONObject.put("duration", myTrack.getDuration());
+                jSONObject.put("src_type", myTrack.getSrcType());
+                jSONObject.put("title", myTrack.getTitle());
+                jSONObject.put("url_content", myTrack.getUrlContent());
+                jSONObject.put("url_thumbnail", myTrack.getUrlThumbnail());
+                jSONObject.put("videoId", myTrack.getVideoID());
+                jSonArray.put(jSONObject); 
             }
+            JSONObject mainObj = new JSONObject();
+            mainObj.put("results", jSonArray);
+            out.print(mainObj.toString());
+
             
-               }    
+
+
+        } catch (JSONException ex) {
+            System.out.println("TEST");
+            ex.printStackTrace();
+            Logger.getLogger(SearchController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
+    private ArrayList<MyTrack> youtubeQuery(String searchString) throws IOException {
+        ArrayList<MyTrack> searchResultsArrayList = new ArrayList<>();
+        MyTrack track;
+        youtube = new YouTube.Builder(new NetHttpTransport(), JSON_FACTORY, new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest request) throws IOException {
+            }
+        }).setApplicationName("youtube-cmdline-search-sample").build();
+        String queryTerm = searchString;
+        YouTube.Search.List search = youtube.search().list("id,snippet");
+        String apiKey = "AIzaSyByS-IwVEMkiCuX2OXvBKdsywjR7Ls7828";
+        search.setKey(apiKey);
+        search.setQ(queryTerm);
+        search.setType("video");
+        search.setFields("items(id/kind,id/videoId,snippet/title,snippet/channelTitle,snippet/thumbnails/default/url)");
+        search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
 
+        SearchListResponse searchResponse = search.execute();
+        List<SearchResult> searchResultList = searchResponse.getItems();
+        if (searchResultList == null) {
+            System.err.println(searchResponse.toString());
+            return null;
+        }
+        for (SearchResult searchResult : searchResultList) {
+            track = new MyTrack();
+            track.setAuthor(searchResult.getSnippet().getChannelTitle());
+            track.setVideoID(searchResult.getId().getVideoId());
+            track.setTitle(searchResult.getSnippet().getTitle());
+            track.setUrlContent("https://www.youtube.com/watch?v=" + searchResult.getId().getVideoId());
+            track.setSrcType("yt");
+            track.setUrlThumbnail(searchResult.getSnippet().getThumbnails().getDefault().getUrl());
+            track.setDuration("3:20");
+            searchResultsArrayList.add(track);
+        }
+        return searchResultsArrayList;
+    }
+
+    private ArrayList<MyTrack> soundCloudQuery(String searchString) {
+        SoundCloud soundcloud = new SoundCloud(
+                "9d562a4a500509e91c35eaaff309690c",
+                "a3a4f1829161169fdab60072892f82e4"
+        );
+        ArrayList<MyTrack> searchResultsArrayList = new ArrayList<>();
+        ArrayList<Track> tracks = soundcloud.findTrack(searchString);
+        MyTrack track;
+        if (tracks == null) {
+            return null;
+        }
+        for (Track trackSC : tracks) {
+            track=new MyTrack();
+            track.setAuthor(trackSC.getUser().getUsername());
+            track.setDuration(trackSC.getDuration().toString());
+            track.setSrcType("sc");
+            track.setTitle(trackSC.getTitle());
+            track.setUrlContent(trackSC.getStreamUrl());
+            track.setUrlThumbnail(trackSC.getArtworkUrl());
+            track.setVideoID(trackSC.getId().toString());
+            searchResultsArrayList.add(track);
+        }
+        
+        return searchResultsArrayList;
+    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     }
-
-
 
 }
---
+++ Runmylist/src/java/pojo/MyTrack.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojo;
+
+/**
+ *
+ * @author Razvan'S PC
+ */
+public class MyTrack {
+
+    private String author;
+    private String title;
+    private String duration;
+    private String videoID;
+    //YouTbe : yt SoundCloud sc
+    private String srcType;
+    private String urlContent;
+    private String urlThumbnail;
+
+    public MyTrack(String author, String title, String duration, String id, String srcType, String urlContent, String urlThumbnail) {
+        this.author = author;
+        this.title = title;
+        this.duration = duration;
+        this.videoID = id;
+        this.srcType = srcType;
+        this.urlContent = urlContent;
+        this.urlThumbnail = urlThumbnail;
+    }
+
+    public MyTrack() {
+    }
+    
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getVideoID() {
+        return videoID;
+    }
+
+    public void setVideoID(String videoID) {
+        this.videoID = videoID;
+    }
+
+    public String getSrcType() {
+        return srcType;
+    }
+
+    public void setSrcType(String srcType) {
+        this.srcType = srcType;
+    }
+
+    public String getUrlContent() {
+        return urlContent;
+    }
+
+    public void setUrlContent(String urlContent) {
+        this.urlContent = urlContent;
+    }
+
+    public String getUrlThumbnail() {
+        return urlThumbnail;
+    }
+
+    public void setUrlThumbnail(String urlThumbnail) {
+        this.urlThumbnail = urlThumbnail;
+    }
+
+    @Override
+    public String toString() {
+        return "MyTrack{" + "author=" + author + ", title=" + title + ", duration=" + duration + ", id=" + videoID + ", srcType=" + srcType + ", urlContent=" + urlContent + ", urlThumbnail=" + urlThumbnail + '}';
+    }
+
+    
+    
+    
+}
